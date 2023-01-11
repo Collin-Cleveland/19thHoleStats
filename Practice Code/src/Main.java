2129a926ca5203d3bf39acdb8ce38fa1473b14ed
Collin Cleveland <116568057+Collin-Cleveland@users.noreply.github.com>
2023-01-11 07:59:54 -0500

    Delete Practice Code directory

--- Practice Code/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] arr = {9,8,7,6,5,4,3,2,1};
-        System.out.println(Arrays.toString(BubbleSort.bubbleSort2(arr)));
-    }
-}
